a672634f82d8234d6986ce3b502b4b7cab554eee
Santiago Reyes <santiago.reyes@platinum.net.co>
2026-05-14 09:02:58 -0500

    (section-12) continue section 12

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,15 +1,22 @@
 import {Controller, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
 import { FilesService } from './files.service';
 import {FileInterceptor} from "@nestjs/platform-express";
+import {fileFilter} from "./helpers/fileFilter.helper";
+import {diskStorage} from "multer";
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file',{
+    fileFilter: fileFilter,
+    storage: diskStorage({
+      destination: './static/uploads'
+    })
+  }))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+    console.log('fileController - ', file)
     return 'hola mundo';
     // return this.filesService.create(createFileDto);
   }
---
+++ src/files/helpers/fileFilter.helper.ts
@@ -0,0 +1,16 @@
+import {BadRequestException} from "@nestjs/common";
+import {ALLOWED_EXTENSIONS} from "../constants/extensions.constant";
+
+export const fileFilter =(req: Express.Request, file: Express.Multer.File, callback: Function) => {
+    console.log('Filter: ', file);
+    if (!file) {
+        callback(new BadRequestException('File is empty'), false);
+        return;
+    }
+    if (!ALLOWED_EXTENSIONS.includes(file.mimetype)) {
+        callback(new BadRequestException('type file not support'), false);
+        return;
+    }
+
+    callback(null, true);
+}
